fe52cfb6423a4b89b23972587f9849b84fa89b12
Teodor G <source@teogor.dev>
2023-06-21 01:51:23 +0300

    a

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,13 +12,27 @@ fun main() {
 
     val type = SudokuType.THREE_BY_THREE
     val difficulty = Difficulty.EASY
-    for (seed in 0L until 10000L) {
+    val startSeed = 0L
+    val endSeed = 2000L
+    val iterationStep = 100L
+    val totalIterations = 2000L / iterationStep
+
+    var stepStartTime = System.currentTimeMillis()
+    for (seed in startSeed until endSeed) {
         val generator = SudokuGenerator(type, seed)
         val sudoku = generator.generate(difficulty)
         sudokus.add(sudoku)
         sudokusData.add(sudoku.puzzle.formatAsString())
 
-        println("sudoku:\n${sudoku.puzzle}")
+        if (seed % iterationStep == 0L) {
+            val endTime = System.currentTimeMillis()
+            val totalTime = endTime - startTime
+            val stepTime = endTime - stepStartTime
+            val step = seed / iterationStep + 1
+
+            println("Step $step/$totalIterations | Time taken: $stepTime ms/${totalTime.toFormattedTime()}")
+            stepStartTime = System.currentTimeMillis()
+        }
     }
 
     val gson = Gson()
@@ -29,6 +43,32 @@ fun main() {
     val endTime = System.currentTimeMillis()
     val generationTime = endTime - startTime
 
+    println()
     println("Sudoku List Generation Time: $generationTime ms")
     println("Sudokus size ${sudokus.size} (${sudokusData.size})")
 }
+
+fun Long.toFormattedTime(): String {
+    val seconds = (this / 1000) % 60
+    val minutes = (this / (1000 * 60)) % 60
+    val hours = (this / (1000 * 60 * 60))
+
+    val timeStringBuilder = StringBuilder()
+
+    if (hours > 0) {
+        timeStringBuilder.append("$hours h ")
+    }
+
+    if (minutes > 0) {
+        timeStringBuilder.append("$minutes m ")
+    }
+
+    if (seconds > 0 && (hours == 0L && minutes == 0L)) {
+        timeStringBuilder.append("$seconds s")
+    } else {
+        timeStringBuilder.append("${this % 1000} ms")
+    }
+
+    return timeStringBuilder.toString().trimEnd(',', ' ')
+}
+
