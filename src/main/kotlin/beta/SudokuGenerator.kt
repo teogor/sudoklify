0fb6a7b33a534e5352d419250d50c0a559bde359
Teodor G <source@teogor.dev>
2023-06-20 23:39:59 +0300

    fixed switch of solved grid with puzzle

--- src/main/kotlin/beta/SudokuGenerator.kt
+++ src/main/kotlin/beta/SudokuGenerator.kt
@@ -2,42 +2,47 @@ package beta
 
 import kotlin.random.Random
 
-class SudokuGenerator(private val numRows: Int, private val numCols: Int, private val seed: Long = System.currentTimeMillis()) {
-    enum class Difficulty { EASY, MEDIUM, HARD }
-
+class SudokuGenerator(
+    private val numRows: Int,
+    private val numCols: Int,
+    private val seed: Long = System.currentTimeMillis()
+) {
+    private val size: Int = numRows * numCols
+    private val boxSize: Int = numCols * numRows
     private val random: Random = Random(seed)
 
+    enum class Difficulty {
+        EASY, MEDIUM, HARD
+    }
+
     fun generate(difficulty: Difficulty): Sudoku {
-        val size = numRows * numCols
         val puzzle = Array(size) { IntArray(size) }
         val solution = Array(size) { IntArray(size) }
 
-        fillDiagonalBoxes(puzzle)
+        fillDiagonalBoxes(puzzle, solution)
         solve(puzzle, solution)
 
         val cluesToRemove = calculateCluesToRemove(difficulty, size)
-        removeClues(puzzle, solution, cluesToRemove)
+        removeClues(puzzle, cluesToRemove)
 
         return Sudoku(puzzle, solution)
     }
 
-    private fun fillDiagonalBoxes(puzzle: Array<IntArray>) {
-        val boxSize = numCols * numRows
-
-        for (boxStart in 0 until boxSize step boxSize) {
-            fillBox(puzzle, boxStart, boxStart)
+    private fun fillDiagonalBoxes(puzzle: Array<IntArray>, solution: Array<IntArray>) {
+        for (boxRow in 0 until numRows) {
+            fillBox(puzzle, solution, boxRow * numCols, boxRow * numCols)
         }
     }
 
-    private fun fillBox(puzzle: Array<IntArray>, startRow: Int, startCol: Int) {
-        val boxSize = numCols * numRows
-        val values = (1..boxSize).toMutableList().shuffled(random)
-
+    private fun fillBox(puzzle: Array<IntArray>, solution: Array<IntArray>, startRow: Int, startCol: Int) {
+        val values = (1..size).toList().shuffled(random)
         var valueIndex = 0
+
         for (row in startRow until startRow + numRows) {
             for (col in startCol until startCol + numCols) {
                 puzzle[row][col] = values[valueIndex]
-                valueIndex++
+                solution[row][col] = values[valueIndex]
+                valueIndex = (valueIndex + 1) % size
             }
         }
     }
@@ -48,16 +53,14 @@ class SudokuGenerator(private val numRows: Int, private val numCols: Int, privat
         val row = emptyCell.row
         val col = emptyCell.col
 
-        val values = (1..numCols * numRows).toMutableList().shuffled(random)
-        for (value in values) {
+        val shuffledValues = (1..size).toList().shuffled(random)
+        for (value in shuffledValues) {
             if (isSafe(puzzle, row, col, value)) {
                 puzzle[row][col] = value
                 solution[row][col] = value
-
                 if (solve(puzzle, solution)) {
                     return true
                 }
-
                 puzzle[row][col] = 0
                 solution[row][col] = 0
             }
@@ -102,9 +105,9 @@ class SudokuGenerator(private val numRows: Int, private val numCols: Int, privat
     }
 
     private fun isValueUniqueInBox(puzzle: Array<IntArray>, startRow: Int, startCol: Int, value: Int): Boolean {
-        for (row in startRow until startRow + numRows) {
-            for (col in startCol until startCol + numCols) {
-                if (puzzle[row][col] == value) {
+        for (row in 0 until numRows) {
+            for (col in 0 until numCols) {
+                if (puzzle[row + startRow][col + startCol] == value) {
                     return false
                 }
             }
@@ -114,78 +117,71 @@ class SudokuGenerator(private val numRows: Int, private val numCols: Int, privat
 
     private fun calculateCluesToRemove(difficulty: Difficulty, size: Int): Int {
         return when (difficulty) {
-            Difficulty.EASY -> (0.35 * size * size).toInt()
-            Difficulty.MEDIUM -> (0.45 * size * size).toInt()
-            Difficulty.HARD -> (0.55 * size * size).toInt()
+            Difficulty.EASY -> size * size / 3
+            Difficulty.MEDIUM -> size * size / 2
+            Difficulty.HARD -> size * size / 1.5.toInt()
         }
     }
 
-    private fun removeClues(puzzle: Array<IntArray>, solution: Array<IntArray>, cluesToRemove: Int) {
-        for (i in 0 until cluesToRemove) {
-            var row: Int
-            var col: Int
-            do {
-                row = random.nextInt(numRows * numCols)
-                col = random.nextInt(numRows * numCols)
-            } while (puzzle[row][col] == 0)
+    private fun removeClues(puzzle: Array<IntArray>, cluesToRemove: Int) {
+        var cluesRemoved = 0
 
-            val removedValue = puzzle[row][col]
-            puzzle[row][col] = 0
+        while (cluesRemoved < cluesToRemove) {
+            val row = random.nextInt(size)
+            val col = random.nextInt(size)
 
-            // Check if the puzzle still has a unique solution after removing the clue
-            val clone = puzzle.copyOf()
-            if (!isPuzzleUnique(clone, solution)) {
-                puzzle[row][col] = removedValue
-            }
-        }
-    }
+            if (puzzle[row][col] != 0) {
+                val temp = puzzle[row][col]
+                puzzle[row][col] = 0
 
-    private fun isPuzzleUnique(puzzle: Array<IntArray>, solvedPuzzle: Array<IntArray>): Boolean {
-        val solutionCount = solve(puzzle, solvedPuzzle)
-        return solutionCount && deepEquals(puzzle, solvedPuzzle)
-    }
-
-    private fun deepEquals(arr1: Array<IntArray>, arr2: Array<IntArray>): Boolean {
-        for (row in arr1.indices) {
-            for (col in arr1[row].indices) {
-                if (arr1[row][col] != arr2[row][col]) {
-                    return false
+                if (!isPuzzleUnique(puzzle)) {
+                    puzzle[row][col] = temp
+                } else {
+                    cluesRemoved++
                 }
             }
         }
-        return true
+    }
+
+    private fun isPuzzleUnique(puzzle: Array<IntArray>): Boolean {
+        val solution = Array(size) { IntArray(size) }
+        val puzzleCopy = Array(size) { IntArray(size) }
+
+        for (i in puzzle.indices) {
+            puzzleCopy[i] = puzzle[i].clone()
+        }
+
+        return solve(puzzleCopy, solution)
     }
 
     private data class Cell(val row: Int, val col: Int)
 }
 
-data class Sudoku(val puzzle: Array<IntArray>, val solution: Array<IntArray>)
+data class Sudoku(
+    val puzzle: Array<IntArray>,
+    val solution: Array<IntArray>
+)
 
 fun main() {
-    val numRows = 4
-    val numCols = 5
-    val seed = 1L
-    val difficulty = SudokuGenerator.Difficulty.EASY
+    val numRows = 3
+    val numCols = 4
+    val seed = 12345L
+    val difficulty = SudokuGenerator.Difficulty.MEDIUM
 
     val generator = SudokuGenerator(numRows, numCols, seed)
     val sudoku = generator.generate(difficulty)
 
-    val puzzle = sudoku.puzzle
-    val solution = sudoku.solution
-
-    // Print the generated puzzle
     println("Generated Puzzle:")
-    printGrid(puzzle)
+    printSudoku(sudoku.puzzle)
 
-    // Print the solution
     println("Solution:")
-    printGrid(solution)
+    printSudoku(sudoku.solution)
 }
 
-private fun printGrid(grid: Array<IntArray>) {
-    for (row in grid.indices) {
-        for (col in grid[row].indices) {
-            print("${grid[row][col]} ")
+fun printSudoku(puzzle: Array<IntArray>) {
+    for (row in puzzle.indices) {
+        for (col in puzzle[row].indices) {
+            print("${puzzle[row][col]} ")
         }
         println()
     }
