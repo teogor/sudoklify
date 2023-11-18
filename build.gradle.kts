29a6db4b0f60c8b546f20da016c4d21c35952d37
Teodor Grigor <open-source@teogor.dev>
2023-11-18 13:40:32 +0200

    Migrate to `teogor/winds` plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import dev.teogor.winds.api.MavenPublish
+import dev.teogor.winds.api.getValue
+import dev.teogor.winds.api.model.Developer
+import dev.teogor.winds.api.model.LicenseType
+import dev.teogor.winds.api.model.Version
+import dev.teogor.winds.api.provider.Scm
+import dev.teogor.winds.gradle.utils.afterWindsPluginConfiguration
+import dev.teogor.winds.gradle.utils.attachTo
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.8.21"
-  id("com.vanniktech.maven.publish") version "0.25.3" apply false
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.winds)
+  alias(libs.plugins.vanniktech.maven)
 }
 
 apply(from = "githooks.gradle.kts")
@@ -10,3 +20,72 @@ apply(from = "githooks.gradle.kts")
 tasks.withType<KotlinCompile>().configureEach {
   dependsOn("updateGitHooks")
 }
+
+winds {
+  buildFeatures {
+    mavenPublish = true
+  }
+
+  mavenPublish {
+    displayName = "Sudoklify"
+    name = "sudoklify"
+
+    canBePublished = false
+
+    description = "Sudoklify stands as a versatile and user-friendly Sudoku puzzle generation library crafted in Kotlin. Effortlessly generate, manipulate, and solve Sudoku puzzles with ease."
+
+    groupId = "dev.teogor.sudoklify"
+
+    artifactIdElements = 1
+
+    inceptionYear = 2023
+
+    sourceControlManagement(
+      Scm.Git(
+        owner = "teogor",
+        repo = "sudoklify",
+      ),
+    )
+
+    version = Version(
+      major = 1,
+      minor = 0,
+      patch = 0,
+    ).setAlphaRelease(4)
+
+    addLicense(LicenseType.APACHE_2_0)
+
+    addDeveloper(TeogorDeveloper())
+  }
+}
+
+afterWindsPluginConfiguration { winds ->
+  val mavenPublish: MavenPublish by winds
+  if (mavenPublish.canBePublished) {
+    mavenPublishing {
+      publishToMavenCentral(SonatypeHost.S01)
+      signAllPublications()
+
+      @Suppress("UnstableApiUsage")
+      pom {
+        coordinates(
+          groupId = mavenPublish.groupId!!,
+          artifactId = mavenPublish.artifactId!!,
+          version = mavenPublish.version!!.toString(),
+        )
+        mavenPublish attachTo this
+      }
+    }
+  }
+}
+
+data class TeogorDeveloper(
+  override val id: String = "teogor",
+  override val name: String = "Teodor Grigor",
+  override val email: String = "open-source@teogor.dev",
+  override val url: String = "https://teogor.dev",
+  override val roles: List<String> = listOf("Code Owner", "Developer", "Designer", "Maintainer"),
+  override val timezone: String = "UTC+2",
+  override val organization: String = "Teogor",
+  override val organizationUrl: String = "https://github.com/teogor",
+) : Developer
--- sudoklify/build.gradle.kts
+++ sudoklify/build.gradle.kts
@@ -13,16 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import com.vanniktech.maven.publish.SonatypeHost
-import dev.teogor.sudoklify.buildlogic.BuildInfo
-
 plugins {
   id("sudoklify.kotlin.library")
-  id("sudoklify.maven.publish.library")
+  alias(libs.plugins.winds)
 }
-
-group = BuildInfo.group.fullName
-version = BuildInfo.version.name
 
 tasks.test {
   useJUnitPlatform()
@@ -32,37 +26,10 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
 }
 
-@Suppress("UnstableApiUsage")
-mavenPublishing {
-  publishToMavenCentral(SonatypeHost.S01)
-
-  signAllPublications()
-
-  coordinates("dev.teogor.sudoklify", "sudoklify", "1.0.0-alpha03")
-
-  pom {
-    name.set("sudoklify")
-    description.set("Sudoklify stands as a versatile and user-friendly Sudoku puzzle generation library crafted in Kotlin. Effortlessly generate, manipulate, and solve Sudoku puzzles with ease.")
-    inceptionYear.set("2023")
-    url.set("https://github.com/teogor/sudoklify/")
-    licenses {
-      license {
-        name.set("The Apache License, Version 2.0")
-        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-        distribution.set("repo")
-      }
-    }
-    developers {
-      developer {
-        id.set("teogor")
-        name.set("Teodor Grigor")
-        url.set("https://github.com/teogor/")
-      }
-    }
-    scm {
-      url.set("https://github.com/teogor/sudoklify/")
-      connection.set("scm:git:https://github.com/teogor/sudoklify.git")
-      developerConnection.set("scm:git:git@github.com:teogor/sudoklify.git")
-    }
+winds {
+  mavenPublish {
+    displayName = "Sudoklify"
+    name = "sudoklify"
+    description = ""
   }
 }
